dba68f3d8a368a9b3e9e746e54b2857ae70bb155
Nabiyevv <maqa.nabiev@gmail.com>
2023-10-17 13:06:50 +0400

    create user model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar',
+        'role',
     ];
 
     /**
@@ -42,4 +44,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class)->orderBy('created_at', 'desc');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class)->orderBy('created_at', 'desc');
+    }
 }
