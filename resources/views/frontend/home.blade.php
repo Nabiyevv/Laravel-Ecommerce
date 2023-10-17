3e6a7329064041a8690742c2ec16eeb41d46e65f
Nabiyevv <maqa.nabiev@gmail.com>
2023-10-17 13:24:05 +0400

    remove services component

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,109 +1,74 @@
 @extends('frontend.layout.layout')
 
 @section('main-content')
-{{-- Hero --}}
-    @include('frontend.partials.home.hero')
+  {{-- Hero --}}
+  @include('frontend.partials.home.hero')
 
-    <div class="site-section site-section-sm site-blocks-1">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="">
-                    <div class="icon mr-4 align-self-start">
-                        <span class="icon-truck"></span>
-                    </div>
-                    <div class="text">
-                        <h2 class="text-uppercase">Free Shipping</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer
-                            accumsan tincidunt fringilla.</p>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="100">
-                    <div class="icon mr-4 align-self-start">
-                        <span class="icon-refresh2"></span>
-                    </div>
-                    <div class="text">
-                        <h2 class="text-uppercase">Free Returns</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer
-                            accumsan tincidunt fringilla.</p>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="200">
-                    <div class="icon mr-4 align-self-start">
-                        <span class="icon-help"></span>
-                    </div>
-                    <div class="text">
-                        <h2 class="text-uppercase">Customer Support</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer
-                            accumsan tincidunt fringilla.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  {{-- Services --}}
+  @include('frontend.partials.home.services')
 
-    <div class="site-section site-blocks-2">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
-                    <a class="block-2-item" href="#">
-                        <figure class="image">
-                            <img src="images/women.jpg" alt="" class="img-fluid">
-                        </figure>
-                        <div class="text">
-                            <span class="text-uppercase">Collections</span>
-                            <h3>Women</h3>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
-                    <a class="block-2-item" href="#">
-                        <figure class="image">
-                            <img src="images/children.jpg" alt="" class="img-fluid">
-                        </figure>
-                        <div class="text">
-                            <span class="text-uppercase">Collections</span>
-                            <h3>Children</h3>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
-                    <a class="block-2-item" href="#">
-                        <figure class="image">
-                            <img src="images/men.jpg" alt="" class="img-fluid">
-                        </figure>
-                        <div class="text">
-                            <span class="text-uppercase">Collections</span>
-                            <h3>Men</h3>
-                        </div>
-                    </a>
-                </div>
+  <div class="site-section site-blocks-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 col-md-6 col-lg-4 mb-lg-0 mb-4" data-aos="fade" data-aos-delay="">
+          <a class="block-2-item" href="#">
+            <figure class="image">
+              <img src="images/women.jpg" alt="" class="img-fluid">
+            </figure>
+            <div class="text">
+              <span class="text-uppercase">Collections</span>
+              <h3>Women</h3>
             </div>
+          </a>
         </div>
+        <div class="col-sm-6 col-md-6 col-lg-4 mb-lg-0 mb-5" data-aos="fade" data-aos-delay="100">
+          <a class="block-2-item" href="#">
+            <figure class="image">
+              <img src="images/children.jpg" alt="" class="img-fluid">
+            </figure>
+            <div class="text">
+              <span class="text-uppercase">Collections</span>
+              <h3>Children</h3>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-6 col-md-6 col-lg-4 mb-lg-0 mb-5" data-aos="fade" data-aos-delay="200">
+          <a class="block-2-item" href="#">
+            <figure class="image">
+              <img src="images/men.jpg" alt="" class="img-fluid">
+            </figure>
+            <div class="text">
+              <span class="text-uppercase">Collections</span>
+              <h3>Men</h3>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
+  </div>
 
-    {{-- Featured Products --}}
-    @include('frontend.partials.home.featured')
-    <div class="site-section block-8">
-        <div class="container">
-            <div class="row justify-content-center  mb-5">
-                <div class="col-md-7 site-section-heading text-center pt-4">
-                    <h2>Big Sale!</h2>
-                </div>
-            </div>
-            <div class="row align-items-center">
-                <div class="col-md-12 col-lg-7 mb-5">
-                    <a href="#"><img src="images/blog_1.jpg" alt="Image placeholder"
-                            class="img-fluid rounded"></a>
-                </div>
-                <div class="col-md-12 col-lg-5 text-center pl-md-5">
-                    <h2><a href="#">50% less in all items</a></h2>
-                    <p class="post-meta mb-4">By <a href="#">Carl Smith</a> <span
-                            class="block-8-sep">&bullet;</span> September 3, 2018</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam iste dolor accusantium facere
-                        corporis ipsum animi deleniti fugiat. Ex, veniam?</p>
-                    <p><a href="#" class="btn btn-primary btn-sm">Shop Now</a></p>
-                </div>
-            </div>
+  {{-- Featured Products --}}
+  @include('frontend.partials.home.featured')
+  <div class="site-section block-8">
+    <div class="container">
+      <div class="row justify-content-center mb-5">
+        <div class="col-md-7 site-section-heading pt-4 text-center">
+          <h2>Big Sale!</h2>
         </div>
+      </div>
+      <div class="row align-items-center">
+        <div class="col-md-12 col-lg-7 mb-5">
+          <a href="#"><img src="images/blog_1.jpg" alt="Image placeholder" class="img-fluid rounded"></a>
+        </div>
+        <div class="col-md-12 col-lg-5 pl-md-5 text-center">
+          <h2><a href="#">50% less in all items</a></h2>
+          <p class="post-meta mb-4">By <a href="#">Carl Smith</a> <span class="block-8-sep">&bullet;</span>
+            September 3, 2018</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam iste dolor accusantium facere
+            corporis ipsum animi deleniti fugiat. Ex, veniam?</p>
+          <p><a href="#" class="btn btn-primary btn-sm">Shop Now</a></p>
+        </div>
+      </div>
     </div>
+  </div>
 @endsection
