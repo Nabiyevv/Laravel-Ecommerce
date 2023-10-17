7668d7ef3b65a4d49bfac503e97d290e08ba233d
Nabiyevv <maqa.nabiev@gmail.com>
2023-10-17 13:23:30 +0400

    add services component

---
+++ resources/views/frontend/partials/home/services.blade.php
@@ -0,0 +1,36 @@
+<div class="site-section site-section-sm site-blocks-1">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-lg-4 d-lg-flex mb-lg-0 mb-4 pl-4" data-aos="fade-up" data-aos-delay="">
+        <div class="icon align-self-start mr-4">
+          <span class="icon-truck"></span>
+        </div>
+        <div class="text">
+          <h2 class="text-uppercase">Free Shipping</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer
+            accumsan tincidunt fringilla.</p>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 d-lg-flex mb-lg-0 mb-4 pl-4" data-aos="fade-up" data-aos-delay="100">
+        <div class="icon align-self-start mr-4">
+          <span class="icon-refresh2"></span>
+        </div>
+        <div class="text">
+          <h2 class="text-uppercase">Free Returns</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer
+            accumsan tincidunt fringilla.</p>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 d-lg-flex mb-lg-0 mb-4 pl-4" data-aos="fade-up" data-aos-delay="200">
+        <div class="icon align-self-start mr-4">
+          <span class="icon-help"></span>
+        </div>
+        <div class="text">
+          <h2 class="text-uppercase">Customer Support</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer
+            accumsan tincidunt fringilla.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
