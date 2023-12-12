d3ead55c578d1f53b586e7be34e14fe47ed2ec3a
Nabiyevv <maqa.nabiev@gmail.com>
2023-12-12 20:34:11 +0400

    Add NavigationComponent and its corresponding view file

---
+++ resources/views/components/navigation-component.blade.php
@@ -0,0 +1,14 @@
+<div>
+  <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+  <div class="bg-light py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 mb-0">
+            <a href="{{ route('homeView') }}">Home</a> 
+            <span class="mx-2 mb-0">/</span>
+            <strong class="text-black">{{ $navName }}</strong>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
