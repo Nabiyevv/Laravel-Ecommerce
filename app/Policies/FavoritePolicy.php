84e199c7cd409bf42430f32e92502bcc7a424106
Nabiyevv <maqa.nabiev@gmail.com>
2023-12-12 20:36:05 +0400

    Add policy classes for Category, Favorite, Product, and User models

---
+++ app/Policies/FavoritePolicy.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Favorite;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class FavoritePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //return $user->role == 'admin';
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Favorite $favorite): bool
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+        return true;
+        return $user->role == 'admin';
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Favorite $favorite): bool
+    {
+        //
+        return $user->role == 'admin';
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Favorite $favorite): bool
+    {
+        //
+        return $user->role == 'admin';
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Favorite $favorite): bool
+    {
+        //
+        return $user->role == 'admin';
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Favorite $favorite): bool
+    {
+        //
+        return $user->role == 'admin';
+    }
+}
