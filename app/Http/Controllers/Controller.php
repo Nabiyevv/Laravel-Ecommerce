a99f437d24612a8de027e10e6b794d8f5cdda77f
Mahammad <maqa.nabiev@gmail.com>
2024-01-01 20:57:13 +0400

    Add caching for featured products in Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Helper\CacheHelper;
 use App\Models\Category;
 use App\Models\Product;
-use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -14,15 +14,19 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    public function index(){
-        
-        return  Category::select(['id','name'])->withCount('products')->get();
+    public function index()
+    {
+        $product = CacheHelper::cacheRemember('featured_products', function () {
+            return Product::all();
+        });
+
+        return "salam";
+        return  Category::select(['id', 'name'])->withCount('products')->get();
 
         return DB::table('categories')
-        ->leftJoin('products', 'categories.id', '=', 'products.category_id')
-        ->select('categories.id', 'categories.name', DB::raw('COUNT(products.id) as product_count'))
-        ->groupBy('categories.id', 'categories.name')
-        ->get();
+            ->leftJoin('products', 'categories.id', '=', 'products.category_id')
+            ->select('categories.id', 'categories.name', DB::raw('COUNT(products.id) as product_count'))
+            ->groupBy('categories.id', 'categories.name')
+            ->get();
     }
 }
- 
